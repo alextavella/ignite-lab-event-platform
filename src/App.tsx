9b676d7ba38b0b22e430feb07ee7e297e720b211
alextavella <alextavella@outlook.com>
2022-06-23 12:18:11 -0300

    feat: create video page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import dayjs from "dayjs";
-import "dayjs/locale/pt-br";
-import { Event } from "./pages/Event";
+import { ApolloProvider } from '@apollo/client'
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+import { BrowserRouter } from 'react-router-dom'
+import { client } from './lib/apollo'
+import { Router } from './Router'
 
-dayjs.locale("pt-br");
+dayjs.locale('pt-br')
 
 function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,26 @@
-import dayjs from "dayjs";
-import { CheckCircle, Lock } from "phosphor-react";
+import dayjs from 'dayjs'
+import { CheckCircle, Lock } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 
 interface LessonProps {
-  title: string;
-  slug: string;
-  availableAt: Date;
-  type: "live" | "class";
+  title: string
+  slug: string
+  availableAt: Date
+  type: 'live' | 'class'
 }
 
 export function Lesson({ title, slug, availableAt, type }: LessonProps) {
-  const isLessonAvailable = dayjs(availableAt).isBefore(dayjs());
+  const isLessonAvailable = dayjs(availableAt).isBefore(dayjs())
 
   const availableDateFormatted = dayjs(availableAt).format(
-    "dddd [•] DD [de] MMMM [•] HH[h]mm"
-  ); // Domingo • 20 de junho • 19h00
+    'dddd [•] DD [de] MMMM [•] HH[h]mm'
+  ) // Domingo • 20 de junho • 19h00
 
   return (
-    <a href={isLessonAvailable ? `#${slug}` : "#"}>
+    <Link to={`/eventos/aula/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -33,12 +34,12 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
             </span>
           )}
           <span className="rounded px-2 py-[0.125rem] text-xs text-green-500 font-bold uppercase border border-green-500">
-            {type === "live" ? `AO VIVO` : `AULA PRÁTICA`}
+            {type === 'live' ? `AO VIVO` : `AULA PRÁTICA`}
           </span>
         </header>
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
-  );
+    </Link>
+  )
 }
