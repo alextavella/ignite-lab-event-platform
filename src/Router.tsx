40036d54d0f8a222ca8c2f33c8a4ffbf7f81fe36
alextavella <alextavella@outlook.com>
2022-06-24 11:38:13 -0300

    feat: create Subscriber page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import { Events } from "./pages/Events";
+import { Route, Routes } from 'react-router-dom'
+import { Subscribe } from './pages/Subscribe'
+import { Events } from './pages/Events'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/eventos" element={<Events />} />
       <Route path="/eventos/aula/:slug" element={<Events />} />
     </Routes>
